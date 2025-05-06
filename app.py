21ca42ae59d5842de4b21caff07de7d9407d6941
Tanvi kanaiya <136492722+Tanvikanaiya@users.noreply.github.com>
2025-05-06 21:09:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,16 +86,16 @@ if st.button("🎯 Recommend"):
         st.error("Movie not found in database.")
     else:
         recommendations = recommend_movies(movie_name, selected_lang, selected_genre)
-        if recommendations:
-    st.success("🔥 Movies You Might Like:")
-    for movie in recommendations:
-        st.markdown(f"### 🎞️ {movie['Title']}")
-        st.write(f"📅 **Release Date:** {movie['Release_Date']}")
-        st.write(f"🌐 **Language:** {movie['Original_Language']}")
-        st.write(f"🎭 **Genre:** {movie['Genre']}")
-        st.write(f"⭐ **Rating:** {movie['Vote_Average']}")
-        st.image(movie['Poster_Url'], use_column_width=True)
-        st.markdown("---")
+       if recommendations:
+           st.success("🔥 Movies You Might Like:")
+           for movie in recommendations:
+                st.markdown(f"### 🎞️ {movie['Title']}")
+                st.write(f"📅 **Release Date:** {movie['Release_Date']}")
+                st.write(f"🌐 **Language:** {movie['Original_Language']}")
+                st.write(f"🎭 **Genre:** {movie['Genre']}")
+                st.write(f"⭐ **Rating:** {movie['Vote_Average']}")
+                st.image(movie['Poster_Url'], use_column_width=True)
+                st.markdown("---")
         else:
             st.warning("No matching recommendations found. Try different filters.")
 
